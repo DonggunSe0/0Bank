18d27c8ba26e98cee5205a94d134320035784ae5
perandowind <perandowind@gmail.com>
2026-06-09 17:46:59 +0900

    Feature/transfer (#18)
    * feature: dto, errorcode, api 엔드포인트 생성
    
    * feature: 입금/송금 서비스 로직 구현
    
    * test: 테스트 코드 작성

---
+++ backend/src/main/java/com/team10/backend/domain/transfer/dto/res/DepositRes.java
@@ -0,0 +1,16 @@
+package com.team10.backend.domain.transfer.dto.res;
+
+import com.team10.backend.domain.transaction.type.TransactionType;
+import java.time.LocalDateTime;
+
+public record DepositRes(
+        Long transactionId,
+        Long accountId,
+        TransactionType type,
+        Long amount,
+        Long balanceBefore,
+        Long balanceAfter,
+        String memo,
+        LocalDateTime transactedAt
+) {
+}
