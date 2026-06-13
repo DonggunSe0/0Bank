d6f1b0dddd52aa9a96891958a1dc0b592feac102
DonggunSe0 <cloudhenry1@gmail.com>
2026-06-13 23:10:13 +0900

    Feature/work 45 young policy domain (#36)
    * chore: youngPolicy Domain 구성
    
    * chore: file 첫문자 대문자로 수정 및 어노테이션 적용
    
    * fix: youngPolicyErrorCode code convetion enum으로 변경
    
    * fix: YoungPolicy entity 식별자 추가
    
    * fix: youngPolicy BaseEntity 상속으로 변경

---
+++ backend/src/main/java/com/team10/backend/domain/youngPolicy/entity/YoungPolicy.java
@@ -0,0 +1,15 @@
+package com.team10.backend.domain.youngPolicy.entity;
+
+import com.team10.backend.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class YoungPolicy extends BaseEntity {
+
+
+}
